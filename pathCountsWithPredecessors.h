e2cd7f0abda24e510b47e5620407f695377b2916
Jędrzej Kubica <jj.kubica@student.uw.edu.pl>
2025-07-08 10:21:27 +0200

    Update pathCountsWithPredecessors.h

--- pathCountsWithPredecessors.h
+++ pathCountsWithPredecessors.h
@@ -1,14 +1,20 @@
 #ifndef _PATHCOUNTSWITHPREDECESSORS_H_
 #define _PATHCOUNTSWITHPREDECESSORS_H_
 
+#include "denseAdjacency.h"
+
 /*
     this module defines the matrix with path counts for predecessors B_k
 */
 
 /*
-
+    predecessorsMatrix
 */
+typedef struct {
+    unsigned int 
 
+} predecessorsMatrix;
 
+predecessorsMatrix *dense2predecessorsMatrix(denseAdjacency dense*);
 
 #endif
